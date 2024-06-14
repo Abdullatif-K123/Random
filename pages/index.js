be973c2932d5ea532902473ee168b32d98fe7a6a
Abdullatif Khayat <abdullatif.khayat123@gmail.com>
2024-06-14 23:01:44 +0300

    night shift

--- pages/index.js
+++ pages/index.js
@@ -21,18 +21,18 @@ export default function Home() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
   const yesButtonSize = noCount * 20 + 16;
-
+ const [choosing, setChoosing] = useState(true);
   const handleNoClick = () => {
     setNoCount(noCount + 1);
   };
   const quotes = [
-    "شايفة هل وردة شقد حلوة اذا شفتيها اضغطي القلب",
-    "شايفة هل وردتين شقد حلوات؟",
-    "شايفة هل تلات ورود شقد حلوين؟",
-    "شايفة كل هل ورد",
-    "بتعرفي اني كنت محسب انوالياقوت الاحمر او الروبي هو احلى واغلى شي بهل دنيا",
-    "شايفة هل قمر شقد حلو ومشرق",
-    "شايفة هل ولد شقد لطيف وحلو",
+    "هل مرة جبتلك بعض الحقائق العلمية المشكوك بصحتها مع شوية شغلات تانية قبل ما تنامي هيك احاول اغيرلك مزاجك",
+    "بانة وقت ينفد صبرها على الاهل والمرضى",
+    "يا علاي يا بانة ما يعرفو انك عم تنامي 4 ساعات او كني اقل كل 30 ساعة لهل شي لما بقلك انت متميزة ما بتصدقي",
+    " ما بعرف بصدق هل معلومة شو رائيك انتي بس منيح قرأتها وعلى كل حال انا مابقلك شي بكفي اجبلك شوكولا ووردة واقلك حقك يا بانة او يا دكتورتي بظن بمشي الحال بس شو رائيك هلأ بهل معلومة صحيحة ولا لا  ",
+    "بانة وقت تعصب مني وانا بقلها زعلتي مني دكتورة, بانة هيك بكون ردها لا واهم شي بتقلي مقدرة الموضوع",
+    "واخيرا عرفت اش عم يصير معي وقت اشوفك ليش بصير غبي نوعا ما طلعت من هدول ال 70",
+    "بس حبيت اغيرلك جو صار وقت عبارات النوم",
   ];
   const getNoButtonText = () => {
     const phrases = [
@@ -50,13 +50,11 @@ export default function Home() {
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
   const imges = [
-    "/flower-one.jpg",
-    "/flower-two.jpg",
-    "/flower-three.jpg",
-    "/flower-four.jpg",
-    "/ruby.webp",
-    "/moon.webp",
-    "/cute-baby.jpg",
+    "/funny-2.jpg",
+    "/fact-2.jpg",
+    "/fact-3.jpg",
+    "/funny-1.jpg",
+    "/know-1.jpg", 
   ];
   const buttonCon = [
     "انتي احلى",
@@ -77,49 +75,50 @@ export default function Home() {
     "اي بدي اضغطو غصباً عنك وعن عيلتك صح انا كيوت بس عنيدة ل ابعد درجة ممكن تتصورها",
   ];
   const handleClick = () => {
-    if (index < 7)
-      setTimeout(() => {
-        Swal.fire({
-          title: buttonCon[index],
-          confirmButtonText:
-            "بعرف وما عم انتظر منك تقلي هل شي لاني بعرف حالي جميلة وحلوة",
-          showClass: {
-            popup: `
-          animate__animated
-          animate__fadeInUp
-          animate__faster
-        `,
-          },
-          hideClass: {
-            popup: `
-          animate__animated
-          animate__fadeOutDown
-          animate__faster
-        `,
-          },
-          customClass: {
-            title: styles.textHead,
-            confirmButton: styles.buttonConfirm,
-          },
-        }).then((result) => {
-          setIndex(index + 1);
-          if (index + 1 === 2) {
-            setBattaryIndesx("/2.webp");
-          }
-          if (index + 1 === 5) {
-            setBattaryIndesx("/3.webp");
-            setImage("/cute-girl.png");
-          }
-          if (index + 1 === 3) setImage("/happy.png");
-          if (index + 1 === 7) {
-            handleFire();
-            setRose(true);
-            sendEmail("Bana has just open the last page");
-          }
-        });
+    // if (index < 7)
+    //   setTimeout(() => {
+    //     Swal.fire({
+    //       title: buttonCon[index],
+    //       confirmButtonText:
+    //         "بعرف وما عم انتظر منك تقلي هل شي لاني بعرف حالي جميلة وحلوة",
+    //       showClass: {
+    //         popup: `
+    //       animate__animated
+    //       animate__fadeInUp
+    //       animate__faster
+    //     `,
+    //       },
+    //       hideClass: {
+    //         popup: `
+    //       animate__animated
+    //       animate__fadeOutDown
+    //       animate__faster
+    //     `,
+    //       },
+    //       customClass: {
+    //         title: styles.textHead,
+    //         confirmButton: styles.buttonConfirm,
+    //       },
+    //     }).then((result) => {
+    //       setIndex(index + 1);
+    //       if (index + 1 === 2) {
+    //         setBattaryIndesx("/2.webp");
+    //       }
+    //       if (index + 1 === 5) {
+    //         setBattaryIndesx("/3.webp");
+    //         setImage("/cute-girl.png");
+    //       }
+    //       if (index + 1 === 3) setImage("/happy.png");
+    //       if (index + 1 === 7) {
+    //         handleFire();
+    //         setRose(true);
+    //         sendEmail("Bana has just open the last page");
+    //       }
+    //     });
 
-        setClick(false);
-      }, 1500);
+    //     setClick(false);
+    //   }, 1500); 
+     setIndex(index + 1)
     handleValantine();
   };
   const sendEmail = async (str) => {
@@ -182,8 +181,8 @@ export default function Home() {
   };
 
   useEffect(() => {
-    sendEmail();
-    handleFire();
+    // sendEmail();
+    // handleFire();
   }, []);
   const handleFire = () => {
     const duration = 15 * 1000,
@@ -200,7 +199,7 @@ export default function Home() {
         ticks = Math.max(200, 500 * (timeLeft / duration));
 
       skew = Math.max(0.8, skew - 0.001);
-
+       
       confetti({
         particleCount: 1,
         startVelocity: 0,
@@ -222,6 +221,14 @@ export default function Home() {
       }
     })();
   };
+
+  const handleButton =(str)=>{
+       setChoosing(false); 
+       sendEmail(str);
+  }
+  useEffect(()=>{
+     index > 5 ? handleFire(): null
+  },[index])
   return (
     <>
       <Head>
@@ -231,43 +238,66 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.content}>
-          <div className={styles.images}>
-            <Image
-              src="/girl_wake.png"
-              width={360}
-              height={360}
-              alt="sad"
-              style={{ borderRadius: "20px" }}
-            />
-          </div>
-          <div className={styles.heartContent}>
-            <p>
-              انا عملت نسخة صباحية مشان اصبح عليكي وعملتها اوتوماتيك بعد
-              الساعة 7ونص الصبح لانو بعرف بهل وقت الشمس بتشرق وبتفيق بس طلع في خطأ ف ما صار هل شي ف انا اسف اني
-              تأأخرت عليكي بأنو اقلك صبح الخير
-            </p>
-            <h1>على كل حال هلأ بعوضك</h1>
-            <h1 style={{ color: "#000" }}>
-              <Typewriter
-                options={{
-                  strings: [
-                    "صباح الخير يا اشطر واذكى واحلا دكتورة بالعالم",
-                    "يردونك تزعلي وانتي دكتورة لك هيبتك ونجاحك ف تبا لمن يريد لك السوء",
-                    "جمعة مباركة عليكي يا بانة",
-                    "شوفيلي هل عبارات شغل غروبات العيلة",
-                    "صبحك الله بالخير، كل ما غرّد الطير،",
-                    "صباح الخير ونسماته، يا قمر الليل ونجماته",
-                    "بس جد مرة التانية بقلك صباح الخير والله يقسملك كل الخير يا بانة يا دكتورتي"               
-                  ],
-                  autoStart: true,
-                  loop: true,
-                  deleteSpeed: 10
+    { index  > 5 && <div className={styles.content}>
+      <div className={styles.images}>
+        <Image
+          src="/girl_sleeping.png"
+          width={360}
+          height={360}
+          alt="sad"
+          style={{ borderRadius: "20px" }}
+        />
+      </div>
+      <div className={styles.heartContent}>
+
+      
+        <h1 style={{ color: "#fff" }}>
+          <Typewriter
+            options={{
+              strings: [
+                "تصبحي على خير يا بانة واحلام سعيدة",
+                "يردونك تزعلي وانتي دكتورة لك هيبتك ونجاحك ف تبا لمن يريد لك السوء",
+                "نامي واستريحي يا بانة ف حتى الزهور يجب ان ترتاح والله ما بعرف شو بدي اكتب المهم وصلت الفكرة مو هيك؟ ",
+                "صبحك الله بالخير، كل ما غرّد الطير،", 
+                "لا تنسي بانة بكرا يوم عرفة كتري دعاء ادعي كتير  لانو جد انا معلق املي بالله ومالي مستسلم بس بالاخير يلي اخترلنا ياه الله رح يصير وانا اسف اذا عم ازعجك بهل شي بس والله عم اترك لو خيط من الامل",
+                "مرة التانية تصبحي على خير يا بانة يا دكتورتي والله يجعلك من نصيبي ويرزقني ويفتح علاي وعليكي واكمل طريقي معكي وافرح بنجاحك واشاركك إنجازاتي"            
+              ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 10
+            }}
+          />
+        </h1>
+      </div>
+    </div>  }
+    { index <=5 &&   <div className={styles.content}>
+       { index <= 5 && <div className={styles.images}>
+              <Image
+                src={imges[index - 1]}
+                width={300}
+                height={300}
+                alt="sleep"
+                style={{borderRadius: "20px"}}
+              /> 
+            </div>}
+      <div className={styles.heartContent}>
+              <h1>
+              {quotes[index]}
+              </h1>
+             { (index===3 && choosing) && <div className={styles.buttonsClicked}>
+              <button  className={styles.yesButton} onClick={()=>{handleButton("صح")}}>صح</button>
+              <button  className={styles.yesButton} style={{background: "green"}}  onClick={()=>{handleButton("عرفت")}}>لو بهمك كنت عرفت لحالك</button>
+              <button  className={styles.noButton}  onClick={()=>{handleButton("غلط")}}>غلط</button>
+              </div>}
+              <Heart
+                isClick={isClick}
+                onClick={() => {
+                  setClick(!isClick);
+                  handleClick();
                 }}
               />
-            </h1>
-          </div>
-        </div>
+            </div>
+        </div>}
         {/* {!rose && (
           <div className={styles.content}>
             <div className={styles.images}>
